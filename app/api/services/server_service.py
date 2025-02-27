111c63c4c8d39fafbe90b0410bb780e527b2b131
Mario Henrique <mariohenriquelp@gmail.com>
2025-02-27 20:55:15 -0300

    adicionando verificação de valor

--- app/api/services/server_service.py
+++ app/api/services/server_service.py
@@ -1,10 +1,17 @@
 import datetime
+from typing import Optional
 from app.api.repositories.server_repository import ServerRepository
 from app.api.services.sensor_service import SensorService
 from app.schemas.server_dto import OutputRegisterDataDTO, PayloadDTO
 
 
 class ServerService:
+
+    async def valite_sensor(temperature: Optional[float], humidity: Optional[float], voltage: Optional[float], current: Optional[float]) -> bool:
+        return any(
+            value is not None
+            for value in [temperature, humidity, voltage, current]
+        )
 
     async def register_data(id:str) -> OutputRegisterDataDTO:
         server_ulid = await ServerService.get_payload_id()
@@ -13,7 +20,12 @@ class ServerService:
         humidity = await SensorService.get_humidity()
         voltage = await SensorService.get_voltage()
         current = await SensorService.get_current()
-
+        
+        if not await ServerService.valite_sensor(temperature, humidity, voltage, current):
+        
+            raise ValueError("At least one sensor value must be provided")
+        
+        
         sensor_data = OutputRegisterDataDTO(
             server_ulid=server_ulid,
             timestamp=timestamp,
