2451f0a180511a6928a40308036e6e402c3c9405
Mario Henrique <mariohenriquelp@gmail.com>
2025-03-05 03:17:01 -0300

    teste

--- tests/test_server_servicce.py
+++ tests/test_server_servicce.py
@@ -1,7 +1,10 @@
+import datetime
 from unittest.mock import AsyncMock, patch
 import pytest
 
+from app.api.models.server_model import ServerModel
 from app.api.repositories.server_repository import ServerRepository
+from app.api.services.server_service import ServerService
 
 
 @pytest.mark.asyncio
@@ -27,3 +30,28 @@ async def test_save_register_data():
             value=value,
             server_time=server_time,
         )
+
+
+@pytest.mark.asyncio
+async def test_get_server_health_by_id_healthy():
+    mock_server = await ServerModel(server_ulid="123", server_name="Test Server")
+
+    with patch.object(
+        ServerService,
+        "get_server_by_id",
+        new_callable=AsyncMock,
+        return_value=mock_server,
+    ):
+        with patch.object(
+            ServerService,
+            "server_time",
+            new_callable=AsyncMock,
+            return_value=datetime,
+        ):
+            with patch.object(ServerService, "server_health", return_value="healthy"):
+                result = await ServerService.get_server_health_by_id("123")
+
+                assert isinstance(result, dict)
+                assert result["server_ulid"] == "123"
+                assert result["status"] == "healthy"
+                assert result["server_name"] == "Test Server"
